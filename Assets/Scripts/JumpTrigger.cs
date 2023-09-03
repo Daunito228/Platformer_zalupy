175a5160e963894bb0b0b0e0bfe6131259e4ef5d
Daunito228 <rep.gep1945@gmail.com>
2023-09-03 18:54:42 +0300

    Double Jump

--- Assets/Scripts/JumpTrigger.cs
+++ Assets/Scripts/JumpTrigger.cs
@@ -7,10 +7,7 @@ public class JumpTrigger : MonoBehaviour
     public Rigidbody2D rb;
     public float jumpForce = 250f;
     private bool isGrounded = false;
-    void Start()
-    {
-        
-    }
+    bool ableToDoubleJump;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -20,8 +17,16 @@ public class JumpTrigger : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || ableToDoubleJump))
         {
+            if (isGrounded)
+            {
+                ableToDoubleJump = true;
+            }
+            else if (ableToDoubleJump)
+            {
+                ableToDoubleJump = false;
+            }
             isGrounded = false;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
