d37b791ddd8f902ccd0ecdb322a2321c9392b03f
Daunito228 <rep.gep1945@gmail.com>
2023-09-05 00:41:01 +0300

    Пропажа пуль при столкновении, видимость игрока врагом

--- Assets/Scripts/BehaviorEnemy.cs
+++ Assets/Scripts/BehaviorEnemy.cs
@@ -10,18 +10,82 @@ public class BehaviorEnemy : MonoBehaviour
     public float speed = 3f;
     private float rotation = 1;
     public static float speedMultiplier = 1;
+    bool isSeePlayer = false;
+    Vector2 trfrm;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void turnEnemy(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Floor")
+        if (collision.gameObject.tag == "Floor")
         {
-        speed *= -1;
-        rotation *= -1;
+            speed *= -1;
+            rotation *= -1;
         }
     }
-    private void FixedUpdate()
+
+    Vector2 DirectionDtrm(float speed)
+    {
+        if (speed > 0)
+        {
+            trfrm = -transform.right;
+        }
+        else
+        {
+            trfrm = transform.right;
+        }
+        return trfrm;
+    }
+
+    void calmBehavior()
     {
         enemy.transform.localScale = new Vector2(rotation, 1);
         rb.velocity = new Vector2(-speed * speedMultiplier, rb.velocity.y);
+    }
+
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isSeePlayer)
+        {
+            turnEnemy(collision);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Ray2D ray = new Ray2D(transform.position, transform.forward);
+            trfrm = DirectionDtrm(speed);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, trfrm);
+            if (hit.transform.name == "Player Cube")
+            {
+                Debug.Log(hit.transform.name);
+                isSeePlayer = true;
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Не видит");
+        isSeePlayer = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isSeePlayer)
+        {
+            calmBehavior();
+        }
+        else
+        {
+            //тут надо дописать приследование игрока. Я заебался
+            Debug.Log("Заметил");
+            trfrm = DirectionDtrm(speed);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, trfrm);
+
+
+        }
     }
 }
--- Assets/Scripts/MoveBullet.cs
+++ Assets/Scripts/MoveBullet.cs
@@ -10,6 +10,11 @@ public class MoveBullet : MonoBehaviour
     {
         direction = MovePlayer.direction;
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
     private void FixedUpdate()
     {
         RbBulletPrefab.velocity = new Vector2(PlayerShot.staticSpeed * direction, RbBulletPrefab.velocity.y);
