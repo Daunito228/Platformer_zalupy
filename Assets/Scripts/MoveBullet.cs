b0e467c064d3c968cec781557ceb48cd137dae91
Daunito228 <rep.gep1945@gmail.com>
2023-09-05 23:02:36 +0300

    НЕ УДАЛЯЙ СТРОКИ А КОММЕНТЬ ИХ БЛЯТЬ. ОНИ МОГУТ ПОТОМ ПОНАДОБИТЬСЯ

--- Assets/Scripts/MoveBullet.cs
+++ Assets/Scripts/MoveBullet.cs
@@ -11,6 +11,11 @@ public class MoveBullet : MonoBehaviour
         direction = MovePlayer.direction;
     }
 
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Destroy(RbBulletPrefab.gameObject);
+    //}
+
     private void FixedUpdate()
     {
         RbBulletPrefab.velocity = new Vector2(PlayerShot.staticSpeed * direction, RbBulletPrefab.velocity.y);
